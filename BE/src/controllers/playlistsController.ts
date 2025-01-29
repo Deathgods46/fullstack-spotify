6d127fb60824bbc87019142f94eb3ad9c0cc61bd
Jaspreet Singh <singh.jaspreet.46@gmail.com>
2025-01-30 01:38:10 +0530

    Be: Added add song to playlists api

--- BE/src/controllers/playlistsController.ts
+++ BE/src/controllers/playlistsController.ts
@@ -33,6 +33,7 @@ export const getUserPlaylists = async (req: Request, res: Response) => {
       songs: playlist.songs.map((song) => ({
         songId: song,
       })),
+      id: playlist._id,
     }));
 
     res.json({
@@ -43,5 +44,49 @@ export const getUserPlaylists = async (req: Request, res: Response) => {
   } catch (error) {
     console.error('Error fetching playlists:', error);
     res.status(500).json({ message: 'Server error', success: false });
+  }
+};
+
+export const addSongToPlaylists = async (req: Request, res: Response) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Unauthorized', success: false });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET_KEY || 'S0M3_$a|/|p|_3',
+    ) as { id: string };
+
+    if (!decoded.id) {
+      return res.status(401).json({ message: 'Invalid token', success: false });
+    }
+
+    const { songId, playlistsId } = req.body;
+    const updatedPlaylists = await Playlist.updateMany(
+      {
+        _id: { $in: playlistsId },
+        user: decoded.id,
+      },
+      {
+        $addToSet: { songs: songId },
+      },
+    );
+
+    if (updatedPlaylists.modifiedCount === 0) {
+      return res.status(404).json({ message: 'No playlists were updated.' });
+    }
+
+    return res
+      .status(200)
+      .json({ message: 'Song added to selected playlists.', success: true });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: 'Server error, please try again later.' });
   }
 };
--- BE/src/routes/playlistRoutes.ts
+++ BE/src/routes/playlistRoutes.ts
@@ -1,11 +1,17 @@
 import { Request, Response, Router } from 'express';
-import { authenticate } from '../middleware/auth';
-import { getUserPlaylists } from '../controllers/playlistsController';
+import {
+  addSongToPlaylists,
+  getUserPlaylists,
+} from '../controllers/playlistsController';
 
 const router = Router();
 
 router.get('/getUserPlaylists', async (req: Request, res: Response) => {
   await getUserPlaylists(req, res);
+});
+
+router.post('/addSongToPlaylists', async (req: Request, res: Response) => {
+  await addSongToPlaylists(req, res);
 });
 
 export default router;
